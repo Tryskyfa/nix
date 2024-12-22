942e864af81ad7156412bce733bdbfa6363e7391
Jan Schlachter <jan.schlachter1@gmail.com>
2024-12-22 01:04:21 +0100

    feat(home-manager): add homemanager

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
 
@@ -24,6 +28,12 @@
 
         modules = [
         ./hosts/desktop/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.tryskyfa = import ./home.nix;
+        }
         ];
       };
     };
