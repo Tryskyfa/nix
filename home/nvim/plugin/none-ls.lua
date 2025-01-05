52973df9da2dc8fc885bb2ffcb729817beec6efa
Jan Schlachter <jan.schlachter1@gmail.com>
2025-01-05 02:01:05 +0100

    feat(nvim): autocmd for formating

--- home/nvim/plugin/none-ls.lua
+++ home/nvim/plugin/none-ls.lua
@@ -1,8 +1,21 @@
 -- note that none-ls is a fork of null-ls, but the api is not changed from null-ls
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
     sources = {
         null_ls.builtins.formatting.nixfmt,
-    }
+    },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
+        end
+    end,
 }
