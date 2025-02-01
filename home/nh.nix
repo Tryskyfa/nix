ab6678e6c9e8064e0d4f393bef9e8e70db34930b
Jan Schlachter <jan.schlachter1@gmail.com>
2025-02-01 16:19:11 +0100

    feat(home): add nh clean settings

--- home/nh.nix
+++ home/nh.nix
@@ -6,7 +6,12 @@
   ];
   programs.nh = {
     enable = true;
-    clean.enable = true;
+    clean = {
+      enable = true;
+      dates = "weekly";
+      extraArgs = "--keep 5 --keep-since 3d";
+    };
     flake = "/home/tryskyfa/nix";
+
   };
 }
