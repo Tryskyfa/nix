aff30fd592897673c1d536ff706967408634b907
Jan Schlachter <jan.schlachter1@gmail.com>
2025-02-02 12:54:50 +0100

    feat(sway): disable focus_follows_mouse

--- home/sway/sway.nix
+++ home/sway/sway.nix
@@ -93,6 +93,7 @@
         input * repeat_rate 40
         input 1133:50503:Logitech_USB_Receiver accel_profile flat
         input * accel_profile adaptive
+        focus_follows_mouse no
       '';
     };
   stylix.targets.swaylock.enable = false;
