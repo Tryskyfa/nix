0d870529ff93adbde81f7b125a15669d213f1d1c
Jan Schlachter <jan.schlachter1@gmail.com>
2025-02-18 18:37:51 +0100

    feat(kanshi): add profile "laptop-docked"

--- home/sway/kanshi.nix
+++ home/sway/kanshi.nix
@@ -23,6 +23,25 @@
           }
         ];
       }
+      {
+        profile.name = "laptop-docked";
+        profile.outputs = [
+          {
+            criteria = "LG Display 0x06ED Unknown";
+            status = "disable";
+          }
+          {
+            criteria = "Dell Inc. AW2521HFA 7LZXL03";
+            position = "0,0";
+            mode = "1920x1080@119.982Hz";
+          }
+          {
+            criteria = "AOC 2460G5 GKAH9HA054177";
+            position = "1920,0";
+            mode = "1920x1080@60Hz";
+          }
+        ];
+      }
     ];
   };
 }
