803fdada6369cf10e185e75ac3fc695c142a6010
Jan Schlachter <jan.schlachter1@gmail.com>
2025-03-25 18:08:14 +0100

    fix(zsh): map autosuggest-accept to <C-M> as there is a issue with <C-I>

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -30,7 +30,7 @@
 
     initExtra = ''
       bindkey "^N" clear-screen
-      bindkey "^I" autosuggest-accept
+      bindkey "^M" autosuggest-accept
 
       # for yazi tui file explorer
       function y() {
