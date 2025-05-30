7ea5ff116cc9eed94d5882eb645e9b799d527af8
Jan Schlachter <jan.schlachter1@gmail.com>
2025-05-30 15:42:38 +0200

    feat(sway): add another keybinding for play pause of media

--- home/sway/sway.nix
+++ home/sway/sway.nix
@@ -95,6 +95,7 @@
           "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
           "XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
           "XF86AudioPlay" = "exec playerctl play-pause";
+          "XF86PickupPhone" = "exec playerctl play-pause";
         };
       };
 
