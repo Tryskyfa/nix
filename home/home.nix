8339acf3bd72054385d51b0eaa714b9281fe57d0
Jan Schlachter <jan.schlachter1@gmail.com>
2025-08-05 22:53:56 +0200

    fix(home): make default applications work

--- home/home.nix
+++ home/home.nix
@@ -98,19 +98,48 @@
 
   xdg.desktopEntries.feh = {
     name = "feh";
-    exec = "${pkgs.feh}/bin/feh";
+    exec = "${pkgs.feh}/bin/feh %f";
     icon = "image";
+    terminal = false;
+    type = "Application";
+    mimeType = [
+      "image/png"
+      "image/jpeg"
+    ];
   };
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
       "application/pdf" = [
-        "zathura.desktop"
-        "firefox.desktop"
+        "org.pwmt.zathura-pdf-mupdf.desktop"
       ];
       "image/png" = [ "feh.desktop" ];
-      "image/jpg" = [ "feh.desktop" ];
+      "image/jpeg" = [ "feh.desktop" ];
       "text/plain" = [ "nvim.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+      "x-scheme-handler/sgnl" = [ "signal.desktop" ];
+      "x-scheme-handler/signalcaptcha" = [ "signal.desktop" ];
+    };
+    associations.added = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
     };
   };
 }
