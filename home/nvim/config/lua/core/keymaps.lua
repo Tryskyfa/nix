23b841c2d710d4a6814b5fbbe96b6617e3e0e98a
Jan Schlachter <jan.schlachter1@gmail.com>
2025-10-14 19:03:23 +0200

    refactor(nvim): move keybinds to plugin config

--- home/nvim/config/lua/core/keymaps.lua
+++ home/nvim/config/lua/core/keymaps.lua
@@ -75,20 +75,3 @@ end, { desc = "Open lazygit", remap = true })
 
 -- mini.surround disable default "s" behavior
 map({ "n", "x" }, "s", "<Nop>")
-
--- load the session for the current directory
-map("n", "<leader>qs", function()
-  require("persistence").load()
-end)
--- select a session to load
-map("n", "<leader>qS", function()
-  require("persistence").select()
-end)
--- load the last session
-map("n", "<leader>ql", function()
-  require("persistence").load({ last = true })
-end)
--- stop Persistence => session won't be saved on exit
-map("n", "<leader>qd", function()
-  require("persistence").stop()
-end)
