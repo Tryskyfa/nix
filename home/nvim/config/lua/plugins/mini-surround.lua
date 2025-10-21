744ac9f219f2c78a7116e8a1b0390184b32b265f
Jan Schlachter <jan.schlachter1@gmail.com>
2025-10-21 16:55:45 +0200

    feat(nvim): change mini surround start key

--- home/nvim/config/lua/plugins/mini-surround.lua
+++ home/nvim/config/lua/plugins/mini-surround.lua
@@ -1,1 +1,14 @@
-require("mini.surround").setup({})
+require("mini.surround").setup({
+  mappings = {
+    add = "`a", -- Add surrounding in Normal and Visual modes
+    delete = "`d", -- Delete surrounding
+    find = "`f", -- Find surrounding (to the right)
+    find_left = "`F", -- Find surrounding (to the left)
+    highlight = "`h", -- Highlight surrounding
+    replace = "`r", -- Replace surrounding
+    update_n_lines = "`n", -- Update `n_lines`
+
+    suffix_last = "l", -- Suffix to search with "prev" method
+    suffix_next = "n", -- Suffix to search with "next" method
+  },
+})
