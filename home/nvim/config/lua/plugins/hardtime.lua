d75c966bacedb2bffe39fe8657d2c9de57934adb
Jan Schlachter <jan.schlachter1@gmail.com>
2025-11-03 14:28:15 +0100

    fix(nvim): disable hardtime when using the neo-tree-popup

--- home/nvim/config/lua/plugins/hardtime.lua
+++ home/nvim/config/lua/plugins/hardtime.lua
@@ -1,3 +1,11 @@
 require("hardtime").setup({
-  disabled_filetypes = { "netrw", "neo-tree", "dapui_scopes", "dapui_breakpoints", "dapui_stacks", "dapui_watches" },
+  disabled_filetypes = {
+    "netrw",
+    "neo-tree",
+    "neo-tree-popup",
+    "dapui_scopes",
+    "dapui_breakpoints",
+    "dapui_stacks",
+    "dapui_watches",
+  },
 })
