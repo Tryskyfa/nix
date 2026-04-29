09e5394c1b42e3ca6889dac78bb22a6da2babae0
Jan Schlachter <jan.schlachter1@gmail.com>
2026-04-29 14:38:52 +0200

    feat(nvim): show some notifications on lualine

--- home/nvim/config/lua/plugins/lualine.lua
+++ home/nvim/config/lua/plugins/lualine.lua
@@ -3,4 +3,15 @@ require("lualine").setup({
     icons_enabled = true,
     globalstatus = true,
   },
+  sections = {
+    lualine_x = {
+      {
+        require("noice").api.status.mode.get,
+        cond = require("noice").api.status.mode.has,
+      },
+      "encoding",
+      "fileformat",
+      "filetype",
+    },
+  },
 })
