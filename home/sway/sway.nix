f5c3a5a8d9f4d6d2d6071e0a42f1abfaf00ef526
Jan Schlachter <jan.schlachter1@gmail.com>
2026-05-14 19:04:44 +0200

    feat(sway): change volume in increments of 5%

--- home/sway/sway.nix
+++ home/sway/sway.nix
@@ -90,8 +90,8 @@
           "${mod}+comma" =
             "exec rofimoji --files nerd_font.csv --selector fuzzel --selector-args=\"--width 120\"";
 
-          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-";
-          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+";
+          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
           "XF86AudioMute" = "exec wpctl set-mute   @DEFAULT_AUDIO_SINK@ toggle";
           "XF86AudioMicMute" = "exec wpctl set-mute   @DEFAULT_AUDIO_SOURCE@ toggle";
           "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
